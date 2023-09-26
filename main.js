d9ebcb2aadbcd651576b8ab7ca0abd0a82650606
Dostonbek Yoqubov <dostonbek.yoqubov9830@gmail.com>
2023-09-26 15:49:15 +0500

    Fix errors

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-// let elTitle = document.querySelector(".user-greeting");
-// let elUserMoney = document.querySelector(".user-money");
-// let elExpenses = document.querySelector(".expenses");
-// let elResult = document.querySelector(".results")
-// let userName = prompt("Ismingizni kiriting");
-// let userMoney = Number(prompt("Sayohat uchun sarflamoqchi bo‘lgan summangizni kiriting"));
-// let userMoneySeperated = userMoney.toLocaleString('en-US');
-// let ticketFare = 500;
-// let hotelFee = 250;
-// let entertainmentFee = 120;
-// let USD_CURRENCY = 10900;
-// let EUR_CURRENCY = 12272;
+let elTitle = document.querySelector(".user-greeting");
+let elUserMoney = document.querySelector(".user-money");
+let elExpenses = document.querySelector(".expenses");
+let elResults = document.querySelector(".results")
+let userName = prompt("Ismingizni kiriting");
+let userMoney = Number(prompt("Sayohat uchun sarflamoqchi bo‘lgan summangizni kiriting"));
+let userMoneySeperated = userMoney.toLocaleString('en-US');
+let ticketFare = 500;
+let hotelFee = 250;
+let entertainmentFee = 120;
+let USD_CURRENCY = 10900;
+let EUR_CURRENCY = 12272;
 
-// elTitle.textContent = `Hurmatli ${userName}`
-// elUserMoney.textContent = `Siz sarflamoqchi bo‘lgan summa: ${userMoneySeperated} so‘m`
-// elExpenses.textContent = `Samalyot bileti: $${Number(ticketFare)} 
-// \nMehmonxona xarajatlari: $${Number(hotelFee)}  
-// \nTurli-xil ko‘ngil ochish uchun: €${Number(entertainmentFee)}`;
+elTitle.textContent = `Hurmatli ${userName}`
+elUserMoney.textContent = `Siz sarflamoqchi bo‘lgan summa: ${userMoneySeperated} so‘m`
+elExpenses.textContent = `Samalyot bileti: $${Number(ticketFare)} 
+\nMehmonxona xarajatlari: $${Number(hotelFee)}  
+\nTurli-xil ko‘ngil ochish uchun: €${Number(entertainmentFee)}`;
 
-// let totalMoney = ticketFare * USD_CURRENCY + hotelFee * USD_CURRENCY + entertainmentFee * EUR_CURRENCY;
+let totalMoney = ticketFare * USD_CURRENCY + hotelFee * USD_CURRENCY + entertainmentFee * EUR_CURRENCY;
 
-// if(userMoney >= totalMoney) {
-//    elResult.textContent = `${userName}, sayohatingiz maroqli o‘tsin`
-// } else {
-//     elResult.textContent = `${userName}, afsuski, siz sayohatga bora olmaysiz`
-// }
+if(userMoney >= totalMoney) {
+   elResults.textContent = `${userName}, sayohatingiz maroqli o‘tsin`
+} else {
+    elResults.textContent = `${userName}, afsuski, siz sayohatga bora olmaysiz`
+}
 
 // let elForm = document.querySelector(".form");
 // let elUserNumber1 = document.querySelector(".user-number1");
@@ -35,10 +35,10 @@
 //     let value1 = elUserNumber1.value;
 //     // elResult.textContent = value1;
 
-    // let numValue1 = elUserNumber1.value;
-    // let numValue2 = elUserNumber2.value;
+//     let numValue1 = elUserNumber1.value;
+//     let numValue2 = elUserNumber2.value;
 
-    // elResult.textContent = Number(numValue1.toLocaleString('en-US')) + Number(numValue2.toLocaleString('en-US'))
+//     elResult.textContent = Number(numValue1.toLocaleString('en-US')) + Number(numValue2.toLocaleString('en-US'))
 // });
 
 
